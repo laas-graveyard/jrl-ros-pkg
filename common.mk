0c869f2b975318f99080ce08f6a501d7bc859d73
Thomas Moulard <thomas.moulard@gmail.com>
2010-12-18 23:50:28 +0100

    Update PKG_CONFIG_PATH in common.mk.

--- common.mk
+++ common.mk
@@ -10,13 +10,19 @@ CMAKE_ARGS = -DCMAKE_INSTALL_PREFIX=$(INSTALL_DIR)/ \
              -DBOOST_ROOT=$(BOOST_ROOT)\
              -DCMAKE_BUILD_TYPE="Release"
 
+PKGCONFIGDIR=install/lib/pkgconfig
 
-DYNAMIC_GRAPH=$(shell rospack find dynamic-graph)/install/lib/pkgconfig
-DG_MIDDLEWARE=$(shell rospack find dg-middleware)/install/lib/pkgconfig
-JRL_MATHTOOLS=$(shell rospack find jrl-mathtools)/install/lib/pkgconfig
-JRL_MAL=$(shell rospack find jrl-mal)/install/lib/pkgconfig
+JRL_MATHTOOLS=$(shell rospack find jrl-mathtools)/$(PKGCONFIGDIR)
+JRL_MAL=$(shell rospack find jrl-mal)/$(PKGCONFIGDIR)
+ABSTRACT_ROBOT_DYNAMICS=\
+ $(shell rospack find abstract-robot-dynamics)/$(PKGCONFIGDIR)
+JRL_DYNAMICS=$(shell rospack find jrl-dynamics)/$(PKGCONFIGDIR)
+JRL_WALKGEN=$(shell rospack find jrl-walkgen)/$(PKGCONFIGDIR)
+DYNAMIC_GRAPH=$(shell rospack find dynamic-graph)/$(PKGCONFIGDIR)
+DG_MIDDLEWARE=$(shell rospack find dg-middleware)/$(PKGCONFIGDIR)
 
-PKG_CONFIG_PATH=$(DG_MIDDLEWARE):$(DYNAMIC_GRAPH):$(JRL_MAL):$(JRL_MATHTOOLS)
+
+PKG_CONFIG_PATH=$(JRL_MATHTOOLS):$(JRL_MAL):$(ABSTRACT_ROBOT_DYNAMICS):$(JRL_DYNAMICS):$(JRL_WALKGEN):$(DYNAMIC_GRAPH):$(DG_MIDDLEWARE)
 
 
 all: install
@@ -28,28 +34,28 @@ $(GIT_DIR):
 
 cmake: $(PACKAGE_NAME)
 	-mkdir -p $(BUILD_DIR)
-	export PKG_CONFIG_PATH=$(PKG_CONFIG_PATH) \
+	export PKG_CONFIG_PATH="$(PKG_CONFIG_PATH)" \
 	 && cd $(BUILD_DIR) \
 	 && $(CMAKE) $(CMAKE_ARGS) $(CMAKE_EXTRA_ARGS) ../$(PACKAGE_NAME)
 
 build: cmake
-	export PKG_CONFIG_PATH=$(PKG_CONFIG_PATH) \
+	export PKG_CONFIG_PATH="$(PKG_CONFIG_PATH)" \
 	 && cd $(BUILD_DIR) && make $(ROS_PARALLEL_JOBS)
 
 install: build
-	export PKG_CONFIG_PATH=$(PKG_CONFIG_PATH) \
+	export PKG_CONFIG_PATH="$(PKG_CONFIG_PATH)" \
 	 && cd $(BUILD_DIR) && make $(ROS_PARALLEL_JOBS) $@
 
 uninstall: cmake
-	export PKG_CONFIG_PATH=$(PKG_CONFIG_PATH) \
+	export PKG_CONFIG_PATH="$(PKG_CONFIG_PATH)" \
 	 && cd $(BUILD_DIR) && make $(ROS_PARALLEL_JOBS) $@
 
 test: build
-	export PKG_CONFIG_PATH=$(PKG_CONFIG_PATH) \
+	export PKG_CONFIG_PATH="$(PKG_CONFIG_PATH)" \
 	 && cd $(BUILD_DIR) && make $(ROS_PARALLEL_JOBS) $@
 
 clean:
-	-export PKG_CONFIG_PATH=$(PKG_CONFIG_PATH) \
+	-export PKG_CONFIG_PATH="$(PKG_CONFIG_PATH)" \
 	 && cd $(BUILD_DIR) && make $(ROS_PARALLEL_JOBS) $@
 
 distclean: clean
